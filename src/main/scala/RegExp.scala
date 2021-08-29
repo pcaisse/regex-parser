ba372d80a7dffdd892f6a8e7bddd3da48541092c
Peter Caisse <pcaisse@gmail.com>
2021-08-28 21:41:20 -0400

    Make types more precise

--- src/main/scala/RegExp.scala
+++ src/main/scala/RegExp.scala
@@ -2,9 +2,12 @@ import cats.data.NonEmptyList
 
 final case class NonSpecialChar(char: Char)
 final case class AnyChar()
-final case class ZeroOrMore(regex: RegExp)
-final case class Or(regexA: RegExp, regexB: RegExp)
-final case class Sequence(regexes: NonEmptyList[RegExp])
+final case class ZeroOrMore(regex: Group | AnyChar | NonSpecialChar)
+final case class Or(regexA: OrRegExp, regexB: OrRegExp)
+final case class Sequence(
+    regexes: NonEmptyList[ZeroOrMore | Group | AnyChar | NonSpecialChar]
+)
 final case class Group(regex: RegExp)
 
+type OrRegExp = Sequence | ZeroOrMore | Group | AnyChar | NonSpecialChar
 type RegExp = NonSpecialChar | AnyChar | ZeroOrMore | Or | Sequence | Group
